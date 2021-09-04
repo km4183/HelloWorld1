c3c8ad3c5c52a4e4b975e12eaa9ea34c0b68ea87
Khaliun Munkhuu <haliunaa@Khaliuns-MacBook-Air.local>
2021-09-03 21:26:32 -0500

    Initial commit

---
+++ src/Day1.java
@@ -0,0 +1,31 @@
+public class Day1 {
+    // variable types
+    //int -> integer
+    //char -> characters
+    // ' -> single quotation mark
+    // " -> double quotation mark
+    //string -> collection of characters
+    //boolean -> unen/hudal (true/false)
+    // = assignment operater
+
+//double: butarhai too
+    public static void main(String[] args){
+        //declare -> zarlan tunhaglah
+        int X = 15;
+        double y = 14.6;
+        char ch = 'a';
+        String word = "hello world";
+        boolean check = true;
+
+        int a = 15;
+        int b = 20;
+        int sum = a + b;
+
+        double u = 15;
+        double v = 20;
+        int something = a * b + sum - a / b;
+
+        System.out.print ("a + b =" + u/v);
+    }
+}
+
